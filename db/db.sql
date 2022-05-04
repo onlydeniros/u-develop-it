e2aca069a3649102389020e3bc08d93a13e8cc3c
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-04 08:15:26 -0400

    seeded in election with data

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,9 @@
-DROP DATABASE IF EXISTS election;
 CREATE DATABASE election;
 USE election;
+
+CREATE TABLE candidates(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    industry_connected BOOLEAN NOT NULL
+);
