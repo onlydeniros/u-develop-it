cf7515ffe4ecabfcf8aff893295e964a78387127
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-04 08:54:39 -0400

    connected express to sql

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const mysql = require('mysql2')
 const app = express();
 
 // Express midddleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Djfox1234!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 
 
