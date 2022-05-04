08dfefb5bb07cb413bbe86ad7df20eb33bedadf8
DeNiro Dumas <denirod@Deniros-MBP.attlocal.net>
2022-05-04 00:01:36 -0400

    used crud

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO candidates(first_name,last_name,industry_connected)
+VALUES
+('Goku','Vegeta',1),
+('Gohan','Trunks',1),
+('Goten','Bulma',0),
+('Krillin','Picilio',1),
+('Supreme','Ki',0),
+('Kakorot','Raddish',0),
+('SpongeBob','Patrick',1),
+('Rick','Morty',1),
+('Drake','Josh',1);
